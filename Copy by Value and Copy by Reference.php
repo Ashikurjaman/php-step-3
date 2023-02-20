6de726b5bb9e3429dc8360f60bb25be9b98dd8ca
Ashikurjaman <ashikbur007@gmail.com>
2023-02-20 19:32:52 +0600

    clr

--- Copy by Value and Copy by Reference.php
+++ Copy by Value and Copy by Reference.php
@@ -1,6 +1,6 @@
 <?php
 
-$d= array(
+/* $d= array(
     'fname'=>'jamanl','lname'=>'kamal')
 ;
 
@@ -8,4 +8,7 @@ $new = $d;
 $new = &$d;  // sallow copy by reference this system copy memory address
 $new ['lname']= 'jaman';  // this call deep copy by value
 print_r($new) ;
-print_r($d) ;
+print_r($d) ; */
+
+
+var_dump(php_ini_loaded_file(), php_ini_scanned_files());
---
+++ Removing-data-from-associative-arrays.php
@@ -0,0 +1,8 @@
+<?php
+
+$person = ['fname'=>'Ashik','lname'=>'jaman'] ;
+
+print_r($person);
+
+unset($person ['lname']);
+print_r($person);
