fd64b0a35e69e683f7d2d12da4facdc51448698a
Ashikurjaman <ashikbur007@gmail.com>
2023-02-22 20:09:18 +0600

    f

---
+++ Reversing-strings.php
@@ -0,0 +1,34 @@
+<?php
+
+
+// $q=array_reverse($y);
+
+// $t=strrev($y[0]);
+// $p=strrev($y[1]);
+
+
+// $e = [$t,$p];
+
+// $z= join(" ",$e);
+
+
+// $length = strlen($y);
+
+// for ($i=$length; $i>=0 ; $i--) { 
+    # code...
+    // echo $y[$i];
+// }
+// $length = strlen($y);
+// for ($i=1; $i<=$length ; $i++) { 
+//     # code...
+//     // echo $y[$i];
+//     $z= $y[$i*-1];
+//     // echo $z;
+// }
+
+// print_r($q);;
+
+$y="Hello Bangladesh";
+
+$f= str_split($y);
+print_r($f);
